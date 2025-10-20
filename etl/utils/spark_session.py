a6545d70c7dd3548e2aa04d3a0478f0e41c17cea
Hassan <ha55ana4bar@gmail.com>
2025-10-20 17:09:15 +1100

    making base logging levels to errors only

--- etl/utils/spark_session.py
+++ etl/utils/spark_session.py
@@ -2,9 +2,12 @@ from pyspark.sql import SparkSession
 
 
 def get_spark(app_name: str = "PosePipleine"):
-    return (
+    spark = (
         SparkSession.builder.appName(app_name)
         .config("spark.sql.session.timeZone", "UTC")
         .config("spark.sql.shuffle.partitions", "4")
         .getOrCreate()
     )
+    # Set log level
+    spark.sparkContext.setLogLevel("ERROR")
+    return spark
